9c906748f8627355703c9f04f371885b1345b939
Jeffrey Cochran <jeffrey.david.cochran@gmail.com>
2025-02-28 07:07:38 -0800

    begin adding quad points

--- src/torch_bspline/bspline.py
+++ src/torch_bspline/bspline.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 
 # Standard
+import warnings
 from typing import Tuple, Optional
 
 import torch
@@ -198,7 +199,115 @@ class BSpline(nn.Module):
             x=x,
             W=W
         )
+    
 
+    @staticmethod
+    def _get_quadrature_points(
+        *,
+        knot,
+        p,
+        x0=None,
+        w0=None,
+        knot0=None,
+        rec=1,
+        newton_max_it=15,
+        newton_tol=1.e-10
+
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Computes optimal quad points per 'Optimal quadrature for univariate and tensor product splines' (CMAME 2016)
+
+        Args
+        ----
+            - knot.......knot vector
+            - p..........polynomial degree
+            - x0.........initial guess
+            - w0.........initial guess for weights
+            - knot0......initial guess
+            - rec........current recursion depth
+            - max_iter...max number of newton iterations
+            - newton_tol.tolerance below which algo is considered converged
+        """
+        n  = len(knot)-p-1  # dimension of our spline space
+        
+        if n % 2==1: # need to have a space of even dimension
+            i = np.where(np.diff(knot) == np.max(np.diff(knot)))  
+            i = i[0][len(i[0])//2]                               # insert new knot in middle of
+            knot = sorted(knot + [np.mean(knot[i:i+2])]) # the largest, centermost knot span
+            n += 1 
+
+        # compute all greville points and integrals (used for initial guess)
+        greville       = np.zeros(n)
+        exact_integral = np.zeros(n)
+        for i in range(n):
+            greville[i]       = np.sum(knot[i+1:i+p+1]) / p 
+            exact_integral[i] = (knot[i+p+1]-knot[i])/(p+1) 
+
+        if x0 is not None: 
+            # if initial guess is provided, use these
+            x = x0
+            w = w0
+        else:           
+            # else compute them based on greville points and integrals
+            w   = (exact_integral[0::2] + exact_integral[1::2])   
+            x   = (      greville[0::2] +       greville[1::2])/2 
+
+        while True:
+            # recursive loop from algorithm 2
+
+            for it in range(newton_max_it): 
+                # newton iteration loop
+
+                N, dN = BSpline(knot, p, x)
+                F  = N @ w - exact_integral 
+
+                dN = dN @ sparse.diags(w)
+                Ni = sorted(list(N.indptr) + list(N.indptr[1:]))
+                N.resize(n,n)
+                N.indptr = np.array(Ni)
+                dNi = sorted(list(dN.indptr) + list(dN.indptr[:-1]))
+                dN.resize(n,n)
+                dN.indptr = np.array(dNi)
+                dF = N + dN
+
+                warnings.filterwarnings("error")
+
+                try:
+                    dx = sparse.linalg.spsolve(dF,-F, use_umfpack=True)
+                except Warning: # singular matrix
+                    warnings.filterwarnings("ignore")
+                    break
+                except RuntimeError: # singular matrix
+                    warnings.filterwarnings("ignore")
+                    break
+                warnings.filterwarnings("ignore")
+                ### umfpack solvers have problems with sequential enumeration (matlab code uses this)
+                # w = w + dx[0:n//2]
+                # x = x + dx[n//2: ]
+                ### interleaved dofs work much better
+                w = w + dx[0::2]
+                x = x + dx[1::2]
+
+                # test for diverging (coarse heuristic, see section 3.3)
+                if( np.min(x)<knot[ 0]): break
+                if( np.max(x)>knot[-1]): break
+
+                # test for converging 
+                if(np.linalg.norm(dx)<newton_tol):  return (w, x, rec, it)
+
+            # at this point, newton iteration has diverged. solve recursively on easier knot
+            if knot0 is not None:
+                w, x, rec, it = _get_quadrature_points((kwargs['knot0'] + knot)/2, p, **kwargs)
+                rec = rec + 1
+                knot0 = (knot0 + knot)/2 
+            else:
+                uniformKnot = np.linspace(knot[0],knot[-1], n+p+1) 
+                w, x, rec, it = getOptimalQuadPoints(uniformKnot, p, rec=1) 
+                kwargs['rec'] = rec
+                kwargs['knot0'] = uniformKnot 
+            kwargs['rec'] += 1 
+            kwargs['x0'] = x 
+            kwargs['w0'] = w 
+        # end loop up and start newton iteration with better initial guess
 
     @staticmethod
     def _bspline_get_values(
